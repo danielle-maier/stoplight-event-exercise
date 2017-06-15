5dd7b0fb82c579c7ad0e39cf1f9986f8cd4aee37
Danielle Maier <daniellemaier@Danielles-MacBook-Pro.local>
2017-06-15 16:09:17 -0600

    ehhhh

--- index.js
+++ index.js
@@ -1,5 +1,51 @@
 (function() {
   'use strict';
 
-  // YOUR CODE HERE
+  var stopButton = document.getElementById('stopButton');
+  var stopLight = document.getElementById('stopLight');
+  stopButton.addEventListener('click', activateStop);
+  stopButton.addEventListener('mouseenter', enter);
+  stopButton.addEventListener('mouseleave', leave);
+  var slowButton = document.getElementById('slowButton');
+  var slowLight = document.getElementById('slowLight');
+  slowButton.addEventListener('click', activateSlow);
+  slowButton.addEventListener('mouseenter', enter);
+  slowButton.addEventListener('mouseleave', leave);
+  var goButton = document.getElementById('goButton');
+  var goLight = document.getElementById('goLight');
+  goButton.addEventListener('click', activateGo);
+  goButton.addEventListener('mouseenter', enter);
+  goButton.addEventListener('mouseleave', leave);
+
+  function enter() {
+    console.log("Entered " + this.textContent + " button");
+  }
+
+  function leave() {
+    console.log("Left " + this.textContent + " button");
+  }
+
+  function activateStop() {
+    if (stopLight.style.background == "red") {
+      stopLight.style.background = "black";
+    } else {
+      stopLight.style.background = "red";
+    }
+  }
+
+  function activateSlow() {
+    if (slowLight.style.background == "yellow") {
+      slowLight.style.background = "black";
+    } else {
+      slowLight.style.background = "yellow";
+    }
+  }
+
+  function activateGo() {
+    if (goLight.style.background == "green") {
+      goLight.style.background = "black";
+    } else {
+      goLight.style.background = "green";
+    }
+  }
 })();
